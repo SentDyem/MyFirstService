ee87e05c6a828766176ed276b311ab4336c31439
SentDyem <sentdyem@my.com>
2019-12-19 17:32:53 +0300

    Second Commit

--- MyFirstService/Service1.cs
+++ MyFirstService/Service1.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace MyFirstService
 {
     public partial class Service1 : ServiceBase
     {
+        Timer timer = new Timer();
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +22,46 @@ namespace MyFirstService
 
         protected override void OnStart(string[] args)
         {
+            WriteToFile("Сервис запущен в " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000;
+            timer.Enabled = true;
         }
 
         protected override void OnStop()
         {
+            WriteToFile("Сервис остановлен в" + DateTime.Now);
         }
+
+        protected void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteToFile("Сервис работает " + DateTime.Now);
+        }
+
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+
+        }
+
     }
 }
